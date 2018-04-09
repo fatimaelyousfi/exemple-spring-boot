d0eebef300bb5d39571d98c494ae3865541514d3
fatima <fatima.elyousfi83@gmail.com>
2018-04-09 17:32:47 +0200

    exemple

--- exemple-spring-boot/src/main/java/dev/exemple/repository/ExempleRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.exemple.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.exemple.entite.Exemple;
-
-public interface ExempleRepository extends JpaRepository<Exemple, Integer> {
-
-}
